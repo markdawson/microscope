90e258e21fad08325596a7832ab5137e5241c0be
Mark Dawson <mark@markdawson.xyz>
2016-08-29 21:59:03 -0400

    removed autopublish

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,5 +1,5 @@
-if (Postsfind().count() === 0) {
-    Post.insert({
+if (Posts.find().count() === 0) {
+    Posts.insert({
         title: 'Introducing Telescope',
         url: 'http://sachagreif.com/introducing-telescope/'
     });
