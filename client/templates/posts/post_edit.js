5b728d9f9da08cf0439d39c1e7dc6289320f40f9
Mark Dawson <mark@markdawson.xyz>
2016-09-02 15:58:42 -0400

    finished adding errors

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,16 @@
+Template.postEdit.onCreated(function() {
+    Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function(field) {
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postEdit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -9,18 +22,21 @@ Template.postEdit.events({
             title: $(e.target).find('[name=title]').val()
         }
 
+        var errors = validatePost(postProperties);
+        if (errors.title || errors.url)
+            return Session.set('postEditErrors', errors);
         // Meteor.call('postEdit', 
         Posts.update(currentPostId, {$set: postProperties}, function(error) {
             if (error) {
                 // display the error to the user
-                alert(error.reason);
+                throwError(error.reason); 
             } else {
                 Router.go('postPage', {_id: currentPostId});
             }
         });
     },
 
-    'click.delete': function(e) {
+    'click .delete': function(e) {
         e.preventDefault();
 
         if (confirm("Delete this post?")) {
